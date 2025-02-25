7ede654b9b4c738e9eefca9e3aa70ec71d562e12
John <jgfriedman99@gmail.com>
2025-02-24 20:58:14 -0800

    checkpoint

--- test.py
+++
@@ -1,7 +0,0 @@
-from secsgml import parse_sgml_submission_into_memory
-
-metadata, docs = parse_sgml_submission_into_memory(filepath='samples/0001193805-23-001296.txt')
-import json
-
-with open('metadata.json', 'w') as f:
-    json.dump(metadata, f, indent=4)
