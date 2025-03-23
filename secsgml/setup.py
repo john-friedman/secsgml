8f19d2a0d1d5a654af8ef305e584e7c09bfc929f
John Friedman <jgfriedman99@gmail.com>
2025-03-22 23:34:26 -0700

    trying to make secsgml compatable

--- secsgml/setup.py
+++ secsgml/setup.py
@@ -2,13 +2,29 @@ from setuptools import setup, Extension
 from pathlib import Path
 import platform
 import os
+import sys
 from setuptools import find_packages
+
+# Handle the Cython dependency - we'll require it to be installed first
+# This block will attempt to import Cython, and if it's not available,
+# it will temporarily install it for the duration of the build
+import subprocess
+import importlib.util
+
+# Check if Cython is installed
+cython_available = importlib.util.find_spec("Cython") is not None
+
+# If Cython is not installed, install it temporarily
+if not cython_available:
+    print("Cython not found, installing temporarily for build...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "cython>=0.29.24"])
+    
+# Now Cython should be available
 from Cython.Build import cythonize
 
 # Platform-specific settings
 include_dirs = []
 library_dirs = []
-
 # Only add Windows paths if on Windows
 if platform.system() == "Windows":
     sdk_paths = [
@@ -31,17 +47,17 @@ try:
 except FileNotFoundError:
     long_description = "# secsgml\nParse Securities and Exchange Commission Standard Generalized Markup Language (SEC SGML) files"
 
-# Define Cython extension with compiler directives
+# Define extensions with .pyx files
 extensions = [
     Extension(
         "secsgml.uu_decode_cy",
-        ["secsgml/uu_decode_cy.pyx"],
+        ["uu_decode_cy.pyx"],
         include_dirs=include_dirs,
         library_dirs=library_dirs,
     ),
     Extension(
         "secsgml.sgml_memory_cy",
-        ["secsgml/sgml_memory_cy.pyx"],
+        ["sgml_memory_cy.pyx"],
         include_dirs=include_dirs,
         library_dirs=library_dirs,
     ),
@@ -56,20 +72,25 @@ cython_directives = {
     'cdivision': True,
 }
 
+# Apply cythonize
+extensions = cythonize(
+    extensions,
+    compiler_directives=cython_directives,
+    annotate=True
+)
+
 setup(
     name="secsgml",
-    version="0.0.7",
+    version="0.0.9",
     packages=find_packages(),
-    install_requires=[
-    ],
+    install_requires=[],
     setup_requires=[
-        'cython',
+        'cython>=0.29.24',
     ],
-    ext_modules=cythonize(
-        extensions,
-        compiler_directives=cython_directives,
-        annotate=True
-    ),
+    package_data={
+        "secsgml": ["*.pyx", "*.c"],  # Include both .pyx and generated .c files
+    },
+    ext_modules=extensions,
     description="Parse Securities and Exchange Commission Standard Generalized Markup Language (SEC SGML) files",
     long_description=long_description,
     long_description_content_type='text/markdown',
